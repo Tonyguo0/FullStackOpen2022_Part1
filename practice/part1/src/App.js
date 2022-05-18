346f96e3afc350624a0208c8052378c9bd0dc14d
Tony Guo <46705969+Tg0o0@users.noreply.github.com>
2022-05-18 21:36:27 +0800

    commit 1.1

--- practice/part1/src/App.js
+++ practice/part1/src/App.js
@@ -1,7 +1,32 @@
-const App = () => (
-  <div>
-    <p>Hello world!</p>
-  </div>
-);
+const Hello = (props) => {
+  return (
+    <div>
+      <p>
+        Hello {props.name} {props.age}
+      </p>
+    </div>
+  );
+};
 
+const App = () => {
+  const name = "Tony";
+  const age = 10;
+  return (
+    <>
+      <h1>Greetings</h1>
+      <Hello name={name} age={age + 14} />
+      <Hello name="Tony1" />
+      <Hello name="Tony22" />
+      <Footer />
+    </>
+  );
+};
+
+const Footer = () => {
+  return (
+    <div>
+      greetings app created by <a href="http://github.com/tonyguo0">tonyguo</a>
+    </div>
+  );
+};
 export default App;
