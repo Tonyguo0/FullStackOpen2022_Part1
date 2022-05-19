83ae80e3604cd195d3bb12a3a8e916c698ff4cd8
AzureAD\TonyGuo <tony.guo@ibm.com>
2022-05-19 15:02:30 +0800

    commit: ex1.5

--- excercises/courseinfo/src/App.js
+++ excercises/courseinfo/src/App.js
@@ -1,25 +1,26 @@
 const App = () => {
-  const course = "Half Stack application development";
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7,
-    },
-    {
-      name: "State of a component",
-      exercises: 14,
-    },
-  ];
-
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   );
 };
@@ -34,7 +35,7 @@ const Header = (props) => {
 };
 
 const Content = (props) => {
-  console.log(props)
+  console.log(props);
   return (
     <>
       <Part part={props.parts[0].name} exercises={props.parts[0].exercises} />
