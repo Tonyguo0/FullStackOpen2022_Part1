13e8711abd57d145feeb849bd005178af0c9070a
Tony Guo <46705969+Tg0o0@users.noreply.github.com>
2022-05-19 08:10:09 +0800

    commit: ex 1.2

--- excercises/courseinfo/src/App.js
+++ excercises/courseinfo/src/App.js
@@ -10,9 +10,14 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
+      <Content
+        part1={part1}
+        exercises1={exercises1}
+        part2={part2}
+        exercises2={exercises2}
+        part3={part3}
+        exercises3={exercises3}
+      />
       <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   );
@@ -27,6 +32,16 @@ const Header = (props) => {
 };
 
 const Content = (props) => {
+  return (
+    <>
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
+    </>
+  );
+};
+
+const Part = (props) => {
   return (
     <>
       <p>
