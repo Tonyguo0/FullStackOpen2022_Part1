413b22b85827191d877fba49f07b65af2b890040
Tony Guo <46705969+Tg0o0@users.noreply.github.com>
2022-05-21 22:30:39 +0800

    Component state, event handlers: Page re-rendering

--- practice/part1/src/App.js
+++ practice/part1/src/App.js
@@ -1,18 +1,26 @@
-const Hello = (props) => {
+const Hello = ({ name, age }) => {
+  /*** can destructure the props object passed to the component function as a parameter */
+  // const {name, age} = props
+  const bornYear = () => new Date().getFullYear() - age;
+
   return (
     <div>
       <p>
-        Hello {props.name} {props.age}
+        Hello {name} {age}
       </p>
+      <p>So you were probably born in {bornYear()}</p>
     </div>
   );
 };
 
-const App = () => {
+const App = (props) => {
+  const { counter } = props;
+
   const name = "Tony";
   const age = 10;
   return (
     <>
+      <h1>{counter}</h1>
       <h1>Greetings</h1>
       <Hello name={name} age={age + 14} />
       <Hello name="Tony1" />
