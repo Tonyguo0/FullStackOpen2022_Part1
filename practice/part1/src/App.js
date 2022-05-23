b6b32793ca1cce15b0782717b18387d4616f8137
Tony Guo <46705969+Tg0o0@users.noreply.github.com>
2022-05-23 22:09:00 +0800

    part1d: conditional Rendering

--- practice/part1/src/App.js
+++ practice/part1/src/App.js
@@ -1,12 +1,30 @@
 import { useState } from "react";
 
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return <div>the app is used by pressing the buttons</div>;
+  }
+  return <div>button press history: {props.allClicks.join(" ")}</div>;
+};
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+);
+
 const App = () => {
-  const [clicks, setClicks] = useState({ left: 0, right: 0 });
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+  const [allClicks, setAll] = useState([]);
 
-  const handleLeftClick = () => setClicks({ ...clicks, left: clicks.left + 1 });
+  const handleLeftClick = () => {
+    setAll(allClicks.concat("L"));
+    setLeft(left + 1);
+  };
 
-  const handleRightClick = () =>
-    setClicks({ ...clicks, right: clicks.right + 1 });
+  const handleRightClick = () => {
+    setAll(allClicks.concat("R"));
+    setRight(right + 1);
+  };
 
   // setTimeout(() => setCounter(counter + 1), 1000);
   // console.log("rendering...", counter);
@@ -14,22 +32,13 @@ const App = () => {
   const age = 10;
   return (
     <>
-      {clicks.left}
-      <button
-        onClick={() => {
-          handleLeftClick();
-        }}
-      >
-        left
-      </button>
-      <button
-        onClick={() => {
-          handleRightClick();
-        }}
-      >
-        right
-      </button>
-      {clicks.right}
+      {left}
+      <Button handleClick={handleLeftClick} text="left" />
+      <Button handleClick={handleRightClick} text="right" />
+
+      {right}
+
+      <History allClicks={allClicks} />
       <Footer />
     </>
   );
