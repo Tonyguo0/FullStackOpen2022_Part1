c77bc60f4dcb632ec1998ea046dede908a84f1a4
Tony Guo <46705969+Tg0o0@users.noreply.github.com>
2022-05-23 21:50:37 +0800

    part1d : Complex state

--- practice/part1/src/App.js
+++ practice/part1/src/App.js
@@ -1,40 +1,12 @@
 import { useState } from "react";
 
-const Hello = ({ name, age }) => {
-  /*** can destructure the props object passed to the component function as a parameter */
-  // const {name, age} = props
-  const bornYear = () => new Date().getFullYear() - age;
-
-  return (
-    <div>
-      <p>
-        Hello {name} {age}
-      </p>
-      <p>So you were probably born in {bornYear()}</p>
-    </div>
-  );
-};
-
-const Display = ({ counter }) => <h1>{counter}</h1>;
-
-const Button = ({ onClick, text }) => {
-  return <button onClick={onClick}>{text}</button>;
-};
-
 const App = () => {
-  const [counter, setCounter] = useState(0);
+  const [clicks, setClicks] = useState({ left: 0, right: 0 });
 
-  const handleClick = () => {
-    console.log("clicked");
-  };
-  const increaseByOne = () => {
-    setCounter(counter + 1);
-  };
-  const decreaseByOne = () => {
-    setCounter(counter - 1);
-  };
+  const handleLeftClick = () => setClicks({ ...clicks, left: clicks.left + 1 });
 
-  const setToZero = () => setCounter(0);
+  const handleRightClick = () =>
+    setClicks({ ...clicks, right: clicks.right + 1 });
 
   // setTimeout(() => setCounter(counter + 1), 1000);
   // console.log("rendering...", counter);
@@ -42,10 +14,22 @@ const App = () => {
   const age = 10;
   return (
     <>
-      <Display counter={counter} />
-      <Button onClick={increaseByOne} text="plus" />
-      <Button onClick={decreaseByOne} text="minus" />
-      <Button onClick={setToZero} text="zero" />
+      {clicks.left}
+      <button
+        onClick={() => {
+          handleLeftClick();
+        }}
+      >
+        left
+      </button>
+      <button
+        onClick={() => {
+          handleRightClick();
+        }}
+      >
+        right
+      </button>
+      {clicks.right}
       <Footer />
     </>
   );
