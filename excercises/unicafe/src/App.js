070dc4f7d5f0424ac196564c4cc5a67f15b5d1c8
Tony Guo <46705969+Tg0o0@users.noreply.github.com>
2022-05-25 20:56:57 +0800

    part1d: ex1.6 read commit

--- excercises/unicafe/src/App.js
+++ excercises/unicafe/src/App.js
@@ -5,7 +5,38 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  return <>code here</>;
+  const goodFeedBack = () => {
+    setGood(good + 1);
+    return good;
+  };
+
+  const neutralFeedBack = () => {
+    setNeutral(neutral + 1);
+    return neutral;
+  };
+
+  const badFeedBack = () => {
+    setBad(bad + 1);
+    return bad;
+  };
+
+  return (
+    <>
+      <h1>give feedback</h1>
+
+      <div>
+        <button onClick={goodFeedBack}>good</button>
+        <button onClick={neutralFeedBack}>neutral</button>
+        <button onClick={badFeedBack}>bad</button>
+      </div>
+
+      <h1>statistics</h1>
+
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </>
+  );
 };
 
 export default App;
