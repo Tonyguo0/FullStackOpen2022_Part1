f5ce418e83807a49c4524dd1673e87926d4c09bb
Tony Guo <46705969+Tg0o0@users.noreply.github.com>
2022-05-25 21:58:04 +0800

    part1d: ex1.8

--- excercises/unicafe/src/App.js
+++ excercises/unicafe/src/App.js
@@ -1,25 +1,6 @@
 import { useState } from "react";
 
-const App = () => {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
-
-  const goodFeedBack = () => {
-    setGood(good + 1);
-    return good;
-  };
-
-  const neutralFeedBack = () => {
-    setNeutral(neutral + 1);
-    return neutral;
-  };
-
-  const badFeedBack = () => {
-    setBad(bad + 1);
-    return bad;
-  };
-
+const Statistics = ({ good, neutral, bad }) => {
   const all = () => {
     return bad + good + neutral;
   };
@@ -34,22 +15,41 @@ const App = () => {
 
   return (
     <>
-      <h1>give feedback</h1>
-
-      <div>
-        <button onClick={goodFeedBack}>good</button>
-        <button onClick={neutralFeedBack}>neutral</button>
-        <button onClick={badFeedBack}>bad</button>
-      </div>
-
-      <h1>statistics</h1>
-
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
       <p>all {all()}</p>
       <p>average {average()}</p>
       <p>positive {positive()}%</p>
+    </>
+  );
+};
+
+const Button = ({ feedback, text }) => {
+  return (
+    <>
+      <button onClick={feedback}>{text}</button>
+    </>
+  );
+};
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  return (
+    <>
+      <h1>give feedback</h1>
+
+      <div>
+        <Button feedback={() => setGood(good + 1)} text="good" />
+        <Button feedback={() => setNeutral(neutral + 1)} text="neutral" />
+        <Button feedback={() => setBad(bad + 1)} text="bad" />
+      </div>
+
+      <h1>statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </>
   );
 };
